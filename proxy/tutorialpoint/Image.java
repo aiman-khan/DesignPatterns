c7de7b8a17fd8c6d467da9cd719c10a18132881d
aiman-khan <53793888+aiman-khan@users.noreply.github.com>
2021-11-23 09:48:28 +0500

    Add files via upload

---
+++ proxy/tutorialpoint/Image.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proxy.tutorialpoint;
+
+/**
+ *
+ * @author FA18-BSE-057
+ */
+public interface Image {
+   void display();
+}
